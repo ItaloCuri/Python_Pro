8071ad39483780ad6f0451eac520cfc74b2272ac
K4Kd3gu4c4m0l3 <italocuri7@gmail.com>
2025-03-14 16:17:09 -0500

    generador de contraseñas seguras

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,21 @@
+import random
+#variable para la contrasenha
 
+caracteres_contrasenha = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+#variable para lo largo de la contrasenha
+
+longitud_contrasenha = int(input("pon la longitud de la contraseña segura que quieres generar:  "))
+
+#contrasenha final
+
+contrasenha = ""
+
+#codigo al azar para sacar la contrasenha
+
+for i in range (longitud_contrasenha):
+    contrasenha += random.choice(caracteres_contrasenha)
+
+#texto final:
+
+print(f"tu contrasenha es:  {contrasenha}")
